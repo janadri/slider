08e5c966ba26d557aaabc548726e23dc85d2a239
janadri <rmas57@yahoo.com>
2016-12-15 23:05:54 -0800

    Create slide.js

---
+++ slide.js
@@ -0,0 +1,12 @@
+  $(window).load(function() { 
+    $('#mbt-c').flexslider({ 
+        animation: "slide", 
+        directionNav: false, 
+         itemWidth: 120, 
+        itemMargin: 25, 
+       touch: true,  
+       slideshowSpeed: 2500, 
+      pauseOnHover: true, 
+     animationSpeed: 700, 
+    }); 
+  }); 
